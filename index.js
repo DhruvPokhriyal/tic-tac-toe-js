a34f3efc70d39bd6f0a72e0f114728c087b881a0
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-11-28 14:25:45 +0530

    Creater winnerCheck function for the game
    This function checks the winner conditions for the given sign
    and return true or false depending on the gameboard

--- index.js
+++ index.js
@@ -6,6 +6,38 @@ const Gameboard = (function () {
         }
         return board;
     }
+    this.winnerCheck = function (player1, player2) {
+        function linearStreak(sign) {
+            let rowStreak = true;
+            let columnStreak = true;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (gameboard[i][j] != sign) {
+                        rowStreak = false;
+                    }
+                    if (gameboard[j][i] != sign) {
+                        columnStreak = false;
+                    }
+                }
+                if (rowStreak || columnStreak) return true;
+            }
+        }
+        function diagonalStreak(sign) {
+            let streak = true;
+            for (let i = 0; i < 3; i++) {
+                if (gameboard[i][i] != sign) {
+                    streak = false;
+                    return streak;
+                }
+            }
+            return streak;
+        }
+        function offDiagonalSearch(sign) {
+            return (
+                gameboard[0][2] == sign && gameboard[1][1] && gameboard[2][0]
+            );
+        }
+    };
     const gameboard = createBoard(3, 3);
     const cross = "X";
     const circle = "O";
@@ -14,4 +46,8 @@ const Gameboard = (function () {
 function player(name, sign) {
     this.name = name;
     this.sign = sign;
+    this.score = 0;
+    this.updateScore = function () {
+        this.score++;
+    };
 }
